ad3b80fdc600ca16937c15973ded210be4697873
liu.haitao <liu.haitao@monstar-lab.com>
2014-03-23 01:54:14 +0900

    修改登录错误，db配置失败

--- config/config_ucenter.php
+++ config/config_ucenter.php
@@ -8,12 +8,12 @@ define('UC_DBUSER', 'deploy');
 define('UC_DBPW', 'beSLVmxYZU');
 define('UC_DBNAME', 'weigou');
 define('UC_DBCHARSET', 'utf8');
-define('UC_DBTABLEPRE', '`daitou`.pre_ucenter_');
+define('UC_DBTABLEPRE', '`weigou`.pre_ucenter_');
 define('UC_DBCONNECT', 0);
 
 define('UC_CHARSET', 'utf-8');
 define('UC_KEY', 'm79fOc02TaY9y3T0G5wb8dycZ3qcdeJ6y4Sco249N5JaNeCfpfValau4x9o98eoe');
-define('UC_API', 'http://localhost/daigou/uc_server');
+define('UC_API', 'http://ec2-54-84-30-79.compute-1.amazonaws.com/uc_server');
 define('UC_APPID', '1');
 define('UC_IP', '');
 define('UC_PPP', 20);
